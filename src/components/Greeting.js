71d026ad3cdae3633424ffde2c7bac7ea01bb00f
kuumaraswamy <nkuumaraswamy@gmail.com>
2022-11-07 17:18:45 +0530

    First test is added

---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Greeting = () => {
+  return (
+    <div>
+      <h2>Hello world !!</h2>
+      <p>It's Good to see You </p>
+    </div>
+  )
+}
+
+export default Greeting
